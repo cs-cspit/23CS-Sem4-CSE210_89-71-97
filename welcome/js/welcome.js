115dbecdeb99cbfe7eca9aaf5b57ab43542e2d35
OM PATEL <148608361+ompatel2109@users.noreply.github.com>
2025-03-28 22:30:07 +0530

    Delete welcome directory

--- welcome/js/welcome.js
+++
@@ -1,9 +0,0 @@
-var startQuizBtn = document.querySelector(".start-quiz-btn");
-
-startQuizBtn.onclick = function(){
-    if("choose subject"){
-        alert("Success");
-    }else{
-        swal("Select Subject !", "Please select subject first !", "warning");
-    }
-}
